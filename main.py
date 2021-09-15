16d7d88b61ad871d5f915deb38426af4fc15461c
libbyleonard22 <815989@mystma.org>
2021-09-15 15:19:40 -0500

    cow cow cow

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ def topFiveFoodTypes():
     #print fifth type
     print ("German")
 topFiveFoodTypes()
+
+def myNameIs(cow):
+    print(cow + " is the name.")
+
+myNameIs("Mia Frykman")
+myNameIs("gibby")
