cdcafd5af16f9057ac6e4ad25feab3ae47bb4458
libbyleonard22 <815989@mystma.org>
2021-09-16 15:19:06 -0500

    madlib music lines

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-def topFiveFoodTypes():
-    #print first type
-    print("Italian")
+def timesTen(num):
+    print(num * 10)
 
-    #print second type
-    print("Mexican")
+timesTen(42)
 
-    #print third type
-    print ("American")
+def addFive(num):
+    print(num + 5)
 
-    #print forth type
-    print("Swedish")
+addFive(9)
 
-    #print fifth type
-    print ("German")
-topFiveFoodTypes()
+def divideByTwo(num):
+    print(num / 2)
 
-def myNameIs(cow):
-    print(cow + " is the name.")
+divideByTwo(23)
 
-myNameIs("Mia Frykman")
-myNameIs("gibby")
+
+def addTwoNumbas(x, y):
+    print(x + y)
+
+addTwoNumbas(-71,3)
+
+
+
+def madLibs(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r):
+    print("While I've stayed at home, I've practiced the + a + for + b + hours every + c")
+
+
+
+madLibs(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r):
