4c57540e58853913e6a095e8c6471da09f6fd114
libbyleonard22 <815989@mystma.org>
2021-09-17 14:27:15 -0500

    Added bottom row madlibs

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ addTwoNumbas(-71,3)
 
 
 def madLibs(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r):
-    print("While I've stayed at home, I've practiced the + a + for + b + hours every + c")
+    print("While I've stayed at home, I've practiced the" + a + "for" + b + "hours every" + c. "My favorite key signature to play and practice in is" + d + e + "It has" + f + "flats and" + g + "sharps. I like this key signature because it is" + h + "and" + i + ". I've also gotten awesome at counting time signatures. The time signature I love is," + j + "because it is" + k + "to count. One instrument I want to learn how to play while I'm at home is" + l + "because it is" + m + "and sounds like a" + n + ". My" + o + "likes it when I play" + p + ", and always gives me a round of applause after my performances! When I return to school, my teacher will be" + q + "of how great I am at playing my instrument. My teacher might even think I sound like" + r + "!")
+)
 
-
-
-madLibs(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r):
+madLibs("kazoo", "fifty", "year", "C", "major", "zero", "zero", "easy", "perfect harmonizing", "fourfour time", "fun", "saxophone", "jazzy", "jazzy cool tune", "fish", "saxophone", "proud", "fifth grade version of me")
